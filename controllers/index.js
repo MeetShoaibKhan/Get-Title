6252d3ae95964a56547629309c230e3f14984bbc
MeetShoaibKhan <meetshoaibkhan1@gmail.com>
2022-09-04 20:18:47 +0500

    done

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,6 @@ const controllers = {};
 
 // controllers.getTitle = require("./getTitles_promises");
 controllers.getTitle = require("./getTitles_async-await");
+controllers.getTitle = require("./getTitles_callbacks");
 
 module.exports = controllers;
